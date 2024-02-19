2c8f78cc7a0c456fb4f8acd350c5fb7e4bf4db92
rameshdumpala1222@gmail.com <arjunaliyas122214@gmail.com>
2024-02-19 17:35:49 +0530

    commiting code

---
+++ gitCode.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const gitCode = () => {
+  return (
+    <div>gitCode</div>
+  )
+}
+
+export default gitCode
---
+++ gitFile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const gitFile = () => {
+  return (
+    <div>gitFile</div>
+  )
+}
+
+export default gitFile
