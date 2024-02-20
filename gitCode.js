185dab6e31cc40e304481b794b351a6974e9db86
rames <rameshdumpala1222@gmail.com>
2024-02-20 17:28:18 +0530

    changes

--- gitCode.js
+++ gitCode.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const gitCode = () => {
   return (
-    <div>gitCode
+    <div>
+      gitCode
       <p>hello Ramesh</p>
+      GitHub is a trusted and secure platform for software development,
+      collaboration, and innovation. It offers features such as GitHub Actions,
+      GitHub Codespaces, GitHub Mobile, GitHub Discussions,
     </div>
-  )
-}
+  );
+};
 
-export default gitCode
+export default gitCode;
