237d8af583656af4925327583ad8a26210e9ce8b
rames <rameshdumpala1222@gmail.com>
2024-02-20 17:47:15 +0530

    change code

--- gitFile.js
+++ gitFile.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const gitFile = () => {
   return (
-    <div>welcome to </div>
+    <div>
+<p>GitHub, Inc. is a developer platform that allows developers to create, store, manage and share their code. Wikipedia
+Headquarters: San Francisco, California, U.S.
+Founder(s): Chris Wanstrath, P. J. Hyett, Scott Chacon, Tom Preston-Werner
+Parent organization: Microsoft</p>
+    </div>
   )
 }
 
